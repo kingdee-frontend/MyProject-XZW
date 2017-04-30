5a27e3c5b8696f9cd8b914adebfa9b927de01363
David <87726263@qq.com>
2017-04-30 18:33:31 +0800

    正则验证

--- public/js/sign-up.js
+++ public/js/sign-up.js
@@ -2,11 +2,11 @@ var app = angular.module("myApp", []);
 app.controller('pageCtrl',['$scope','$http','$interval',function($scope,$http,$interval){
 	window.$scope=$scope
 	$scope.warnOn = function(){
-		var len = $scope.user.content.length;
-		if(len>16){
-			$scope.user.long = true;
-		}else{
-			$scope.user.long = false;
-		}
+		// var len = $scope.user.content.length;
+		// if(len>16){
+		// 	$scope.user.long = true;
+		// }else{
+		// 	$scope.user.long = false;
+		// }
 	}
 }]);
